7fa200f6523a106a3cd54a062bca2b1d7888cb35
John Kim <infend@gmail.com>
2021-04-16 22:00:57 +0900

    a

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PIL import Image
 
 import colorizer
 
+model = colorizer.load_model()
+
 st.title('Deoldify')
 
 uploaded_file = st.file_uploader("Choose a brain MRI ...", type=["jpg", "jpeg", "png"])
@@ -10,8 +12,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='원본 이미지', use_column_width=True)
     st.write("")
-    #result = st.write("변환중 ...")
-    model = colorizer.load_model()
+    # result = st.write("변환중 ...")
     filtered_image = colorizer.convert(model, image)
 
     st.image(filtered_image, caption='변환 이미지', use_column_width=True)
