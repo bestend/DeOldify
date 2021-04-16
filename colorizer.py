a7c8c0c91c439883e0602c6e3d6796c19d1e9614
John Kim <infend@gmail.com>
2021-04-16 21:45:25 +0900

    aa

--- colorizer.py
+++ colorizer.py
@@ -5,16 +5,17 @@ from deoldify.device_id import DeviceId
 from deoldify.visualize import *
 from utils import download
 
+download("https://download.pytorch.org/models/resnet34-333f7ec4.pth", "resnet34-333f7ec4.pth",
+         target_dir=os.path.expanduser('~/.cache/torch/hub/checkpoints/'))
+download("https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth", "ColorizeArtistic_gen.pth",
+         target_dir='models')
+
 
 @st.cache(allow_output_mutation=True)
 def load_model():
     print('load model')
     device.set(device=DeviceId.GPU0)
     warnings.filterwarnings("ignore", category=UserWarning, message=".*?Your .*? set is empty.*?")
-    download("https://download.pytorch.org/models/resnet34-333f7ec4.pth", "resnet34-333f7ec4.pth",
-             target_dir=os.path.expanduser('~/.cache/torch/hub/checkpoints/'))
-    download("https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth", "ColorizeArtistic_gen.pth",
-             target_dir='models')
 
     model = get_image_colorizer(artistic=True)
     return model
